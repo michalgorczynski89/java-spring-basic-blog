81a949db05e6052f4aef2397b9fbae4e9ebbd05a
Michal Gorczynski <michal.gorczynski@thalesgroup.com>
2022-01-12 20:09:00 +0100

    atfer second part

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,16 +1,27 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
     @RequestMapping(value = "/")
     public String listPosts(ModelMap modelMap){
-        modelMap.put("title", "Blog Post 1");
+        final List<Post> allPosts = postRepository.getAllPosts();
+        modelMap.put("posts", allPosts);
         return "home";
+    }
+
+    public BlogController(PostRepository postRepository){
+        this.postRepository = postRepository;
     }
 
 }
